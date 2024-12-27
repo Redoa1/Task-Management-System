79d678cc645716a3b21f32cca19eb2d32d37661a
Redoan Ahmed <redoancseiiuc@gmail.com>
2024-12-28 02:32:56 +0600

    API Add

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/tasks', [TaskController::class, 'index']);
 Route::middleware('auth:sanctum')->post('/tasks', [TaskController::class, 'store']);
